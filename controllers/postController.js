a45d186ccc19ef25f02dfdd65504414f294ddba9
Nick Vicente <nicolas.a.vicente44@gmail.com>
2023-12-04 13:34:41 -0500

    get post modified

--- controllers/postController.js
+++ controllers/postController.js
@@ -122,6 +122,12 @@ export const getPost = async (req, res, next) => {
             match: {
               check: true,
             },
+            populate: [
+              {
+                path: "user",
+                select: ["avatar", "name"],
+              },
+            ],
           },
         ],
       },
@@ -139,15 +145,47 @@ export const getPost = async (req, res, next) => {
 };
 
 export const getAllPosts = async (req, res, next) => {
+  //filter blog results
   try {
-    const posts = await Post.find({}).populate([
-      {
-        path: "user",
-        select: ["avatar", "name", "verified"],
-      },
-    ]);
+    const filter = req.query.searchKeyword;
+    let where = {};
+    if (filter) {
+      where.title = { $regex: filter, $options: "i" };
+    }
 
-    res.json(posts);
+    //limit blog results
+    let query = Post.find(where);
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.limit) || 10; //default value
+    const skip = (page - 1) * pageSize;
+    const total = await Post.find(where).countDocuments();
+    const pages = Math.ceil(total / pageSize);
+
+    if (page > pages) {
+      const error = new Error("No page found");
+      return next(error);
+    }
+
+    const result = await query
+      .skip(skip)
+      .limit(pageSize)
+      .populate([
+        {
+          path: "user",
+          select: ["avatar", "name", "verified"],
+        },
+      ])
+      .sort({ updatedAt: "desc" }); //sort results
+
+    res.header({
+      "x-filter": filter,
+      "x-totalcount": JSON.stringify(total),
+      "x-currentpage": JSON.stringify(page),
+      "x-pagesize": JSON.stringify(pageSize),
+      "x-totalpagecount": JSON.stringify(pages),
+    });
+
+    return res.json(result);
   } catch (error) {
     next(error);
   }
